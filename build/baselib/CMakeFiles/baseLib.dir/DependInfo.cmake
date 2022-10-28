bdb57442deb6be0edbafdf83417b2dd956a79c93
gp <carlosguo@foxmail.com>
2022-10-27 23:08:32 -0700

    编译版本

---
+++ build/baselib/CMakeFiles/baseLib.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gp/myproject/TLS_MemoryPool/baselib/CentralCache.cpp" "/home/gp/myproject/TLS_MemoryPool/build/baselib/CMakeFiles/baseLib.dir/CentralCache.cpp.o"
+  "/home/gp/myproject/TLS_MemoryPool/baselib/PageCache.cpp" "/home/gp/myproject/TLS_MemoryPool/build/baselib/CMakeFiles/baseLib.dir/PageCache.cpp.o"
+  "/home/gp/myproject/TLS_MemoryPool/baselib/ThreadCache.cpp" "/home/gp/myproject/TLS_MemoryPool/build/baselib/CMakeFiles/baseLib.dir/ThreadCache.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../baselib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
